3dc909d1cf1d2aff362d5a2a9682ae276d1bc3c2
Ariel <Ariel@Maddies-MacBook-Pro.local>
2020-11-02 23:00:35 -0500

    [Sprites] Added player sprite, as ability to move left and right. Enemy sprite added

--- app.js
+++ app.js
@@ -1,53 +1,85 @@
-const shipImg = "Ship1.jpg";
 const healthDisplay = document.getElementById("game-lives");
 const score = document.getElementById("game-score");
 const startButton = document.getElementById("game-start");
 const game = document.getElementById("game");
 
+
+const image = document.getElementById('user');
+const image2 = document.getElementById('enemy');
 const computedStyle = getComputedStyle(game);
 const height = computedStyle.height;
 const width = computedStyle.width;
-// game.height = height.replace('px', '');
-// game.width = width.replace('px', '');
 
 const ctx = game.getContext('2d');
 
-// const ship = document.createElement("img");
 
 
-
-
-class Sprite {
-    constructor(x, y, color, width, height) {
+class Aliens {
+    constructor(x, y, width, height) {
         this.x = x
         this.y = y
-        this.color = color
         this.width = width
         this.height = height
         this.alive = true
     }
 
     render() {
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx.drawImage(image2, this.x, this.y, this.width, this.height)
     }
 }
 
-const player = new Sprite(143, 120, 'white', 15, 15)
-console.log(player)
+
+const alien = new Aliens(140, 10, 20, 20)
+
+
+
+// const arrAliens = [];
+// for (let row = 0; row < 3; row++) {
+//     for(let col = 0; col < 4; col++) {
+//         const alien = new Aliens ({
+//             x: col * 20,
+//             y: row * 20,
+//         })
+//         arrAliens.push[alien]
+//     }
+// }
+
+
+
+
+class Sprite {
+    constructor(x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.alive = true
+    }
+
+    render() {
+        ctx.drawImage(image, this.x, this.y, this.width, this.height)
+    }
+}
+
+
+const player = new Sprite(138, 125, 24, 16)
 
 document.getElementById('game-start').addEventListener('click', function() {
     player.render();
-    setInterval(rePaint, 1000/200)
+    alien.render()
+    setInterval(rePaint, 1000/500)
 })
 
-document.addEventListener('keyup', function(evt){
-    if (evt.key === 'd' && player.x < 284){
+
+
+document.addEventListener('keydown', function(evt) {
+    if (evt.key === 'ArrowRight' && player.x < 270){
         player.x += 10
-    } else if (evt.key === 'a' && player.x > 3) {
+    } else if (evt.key === 'ArrowLeft' && player.x > 6) {
         player.x -= 10
+    } else if (evt.keyCode === 32) {
+    console.log("shoot")
     }
-    console.log(player)
 })
 
 
@@ -55,8 +87,9 @@ function rePaint() {
     ctx.clearRect(0, 0, game.width, game.height)
 
     player.render()
+    alien.render()
     
 }
 
- //60 fps
+
    
